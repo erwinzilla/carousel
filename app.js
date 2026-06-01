726cfc7b60860a4109087a5ab216db9146ae6c38
erwinzilla <annrwinz@gmail.com>
2026-06-01 12:04:54 +0700

    fix some issues

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const { downloadFile } = require("./services/download");
 const { generatePdf } = require("./services/generatePdf");
@@ -15,9 +16,10 @@ app.post("/print-job", async (req, res) => {
 
     const tempDir = path.join(__dirname, "temp");
 
-    const job1Path = path.join(tempDir, "job-1.pdf");
-    const job2Path = path.join(tempDir, "job-2.pdf");
-    const finalPath = path.join(tempDir, "job-final.pdf");
+    const unique = `${jobId}-${Date.now()}-${Math.random().toString(16).slice(2)}`;
+    const job1Path = path.join(tempDir, `job-${unique}-1.pdf`);
+    const job2Path = path.join(tempDir, `job-${unique}-2.pdf`);
+    const finalPath = path.join(tempDir, `job-final-${unique}.pdf`);
 
     // 🔥 1. Download Cakrawala (via Laravel proxy)
     const job1Url = `https://erwinzilla.com/api/cakrawala/invoices/${jobId}/nota-invoice`;
@@ -35,14 +37,39 @@ app.post("/print-job", async (req, res) => {
     // 🔥 4. Print
     // await printFile(finalPath);
 
-    // res.json({ status: "printed" });
-    res.json({
-      status: "success",
-      file: finalPath,
+    if (!fs.existsSync(finalPath)) {
+      return res.status(500).json({ error: "PDF_NOT_FOUND" });
+    }
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename=job-final-${unique}.pdf`,
+    );
+    res.sendFile(finalPath, (err) => {
+      if (err) {
+        console.error("SendFile error:", err);
+      }
+
+      fs.unlink(job1Path, (err) => {
+        if (err) console.error("Failed delete job1:", err);
+      });
+      fs.unlink(job2Path, (err) => {
+        if (err) console.error("Failed delete job2:", err);
+      });
+      fs.unlink(finalPath, (err) => {
+        if (err) console.error("Failed delete job-final:", err);
+      });
     });
+
+    // res.json({ status: "printed" });
+    // res.json({
+    //   status: "success",
+    //   file: finalPath,
+    // });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "FAILED_PRINT" });
+    res.status(500).json({ error: "ERROR_GENERATE_PDF" });
   }
 });
 
@@ -50,4 +77,4 @@ app.listen(3000, () => {
   console.log("Print Service running on port 3000");
 });
 
-app.use("/files", express.static("temp"));
+// app.use("/files", express.static("temp"));
