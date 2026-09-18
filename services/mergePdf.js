fbc04e2767a11201d507bebe1bdefa418cfa9e87
erwinzilla <annrwinz@gmail.com>
2026-09-18 14:58:08 +0700

    hanya halaman 3 wo

--- services/mergePdf.js
+++ services/mergePdf.js
@@ -8,12 +8,17 @@ async function mergePdf(job1Path, job2Path, outputPath) {
   const finalPdf = await PDFDocument.create();
 
   // halaman 1 dari job-1
-  const [page1] = await finalPdf.copyPages(pdf1, [0]);
-  finalPdf.addPage(page1);
+  // const [page1] = await finalPdf.copyPages(pdf1, [0]);
+  // finalPdf.addPage(page1);
 
   // semua halaman dari job-2
-  const pages2 = await finalPdf.copyPages(pdf2, pdf2.getPageIndices());
-  pages2.forEach((p) => finalPdf.addPage(p));
+  // const pages2 = await finalPdf.copyPages(pdf2, pdf2.getPageIndices());
+  // pages2.forEach((p) => finalPdf.addPage(p));
+
+  // sementara ubah logic dulu
+  // hanya ambil halaman 3
+  const [page1] = await finalPdf.copyPages(pdf2, [1]);
+  finalPdf.addPage(page1);
 
   const bytes = await finalPdf.save();
   fs.writeFileSync(outputPath, bytes);
